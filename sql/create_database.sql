517e62ebe3ac371f0684976dbbfd05331e64a938
adrien.rmd <adrien.remond@iut.local>
2026-09-17 10:42:09 +0200

    debut base de donné

---
+++ sql/create_database.sql
@@ -0,0 +1,5 @@
+-- Création de la base de données birthday_reminder
+CREATE DATABASE skillswap;
+-- Affichage d'un message de confirmation
+DO $$ BEGIN RAISE NOTICE 'Base de donnees skillswap creee avec succes !';
+END $$;
